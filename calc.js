5c75a52afacf77798b435db06c59d42590741347
shyam patel <shyamkamaniya2406@gmail.com>
2022-05-04 12:24:44 +0530

    changes

--- calc.js
+++ calc.js
@@ -2,5 +2,20 @@ function add(a,b) {
     console.log(a+b);   
 }
 
+function sub(x,y){
+    console.log(x-y);
+}
+
+function mul(x,y){
+    console.log(x*y);
+}
+
+function div(x,y){
+    console.log(x/y);
+}
+
 
 module.exports.addition = add
+module.exports.sub = sub
+module.exports.mul = mul
+module.exports.div = div
--- oldindex.js
+++ oldindex.js
@@ -1,3 +1,9 @@
 const calc = require("./calc")
 
 calc.addition(25,12)
+calc.sub(50,21)
+calc.mul(14,38)
+calc.div(8,4)
+
+
+
